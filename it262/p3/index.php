38cf93e47f4563af89404bfa39406a65f61fbbff
Natasha <natasha.d.moore@seattlecolleges.edu>
2023-02-09 19:09:09 -0800

    updated file names, added white space to index

--- it262/p3/index.php
+++ it262/p3/index.php
@@ -7,36 +7,41 @@
     <title>For Your Special Burger</title>
 </head>
 <body>
-<h1>Burger Truck</h1>
-    <form action="<?php echo $_SERVER['PHP_SELF']   ;?>" method="post">
-<fieldset>
-<label>NAME</label>
-<input type="text" name="name" value="<?php if(isset($_POST['name'])) echo htmlspecialchars($_POST['name']) ;?>">
 
-<label>Burger</label>
-<input type="number" name="burger" value="<?php if(isset($_POST['burger'])) echo htmlspecialchars($_POST['burger']) ;?>">
+   <h1>Burger Truck</h1>
 
-<label>Salad</label>
-<input type="number" name="salad" value="<?php if(isset($_POST['salad'])) echo htmlspecialchars($_POST['salad']) ;?>">
+      <form action="<?php echo $_SERVER['PHP_SELF'] ;?>" method="post">
 
-<label>French Fries</label>
-<input type="number" name="fries" value="<?php if(isset($_POST['fries'])) echo htmlspecialchars($_POST['fries']) ;?>">
+         <fieldset>
 
-<label>Chicken Nugget</label>
-<input type="number" name="nugget" value="<?php if(isset($_POST['nugget'])) echo htmlspecialchars($_POST['nugget']) ;?>">
+            <label>NAME</label>
+            <input type="text" name="name" value="<?php if(isset($_POST['name'])) echo htmlspecialchars($_POST['name']) ;?>">
 
-<label>Coke</label>
-<input type="number" name="coke" value="<?php if(isset($_POST['coke'])) echo htmlspecialchars($_POST['coke']) ;?>">
+            <label>Burger</label>
+            <input type="number" name="burger" value="<?php if(isset($_POST['burger'])) echo htmlspecialchars($_POST['burger']) ;?>">
 
-<label>Any other requirement</label>
-<input type="text" name="other" value="<?php if(isset($_POST['other'])) echo htmlspecialchars($_POST['other']) ;?>">
+            <label>Salad</label>
+            <input type="number" name="salad" value="<?php if(isset($_POST['salad'])) echo htmlspecialchars($_POST['salad']) ;?>">
 
-<input type="submit" value="Place the order">
+            <label>French Fries</label>
+            <input type="number" name="fries" value="<?php if(isset($_POST['fries'])) echo htmlspecialchars($_POST['fries']) ;?>">
 
-</fieldset>
-<p><a href="">Reset</a></p>
+            <label>Chicken Nugget</label>
+            <input type="number" name="nugget" value="<?php if(isset($_POST['nugget'])) echo htmlspecialchars($_POST['nugget']) ;?>">
 
-</form>
+            <label>Coke</label>
+            <input type="number" name="coke" value="<?php if(isset($_POST['coke'])) echo htmlspecialchars($_POST['coke']) ;?>">
+
+            <label>Any other requirement</label>
+            <input type="text" name="other" value="<?php if(isset($_POST['other'])) echo htmlspecialchars($_POST['other']) ;?>">
+
+            <input type="submit" value="Place the order">
+
+         </fieldset>
+         
+         <p><a href="">Reset</a></p>
+
+      </form>
 
 <?php
 
